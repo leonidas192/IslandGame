2908173d7299e66c53a18fd0e1d0b8a76eacd284
leonidas192 <kevinmaulana914@gmail.com>
2020-12-25 05:40:03 +0700

    Bab 10 Selesai

--- Island Game/Assets/Scripts/FootStepSoundPlayer.cs
+++ Island Game/Assets/Scripts/FootStepSoundPlayer.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class FootStepSoundPlayer : MonoBehaviour
 {
     public AudioSource footStepAudioSource;
-    public AudioClip footStepClip;
+    public AudioClip footStepClip, waterStepClip;
+    private AudioClip currentStepSound;
     float lastTime = 0;
     float duration;
 
     private void Start()
     {
         duration = footStepClip.length;
+        currentStepSound = footStepClip;
     }
 
     public void PlayFootStepSound()
     {
         if(lastTime == 0)
         {
-            footStepAudioSource.PlayOneShot(footStepClip);
+            footStepAudioSource.PlayOneShot(currentStepSound);
         }
         if(Time.time - lastTime >= duration)
         {
             lastTime = Time.time;
-            footStepAudioSource.PlayOneShot(footStepClip);
+            footStepAudioSource.PlayOneShot(currentStepSound);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "water")
+        {
+            currentStepSound = waterStepClip;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "water")
+        {
+            currentStepSound = footStepClip;
         }
     }
 }
